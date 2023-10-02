98bc6245a54fd505ad94900adf91507efcd4627e
gabeFrancisco <gabegit2@gmail.com>
2023-10-01 22:20:55 -0300

    fixed Login on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Logo from '../../public/drkey.png'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
       <Image src={Logo} alt='Logo'/>
       <h1 className='text-2xl'>Bem vindo ao <b>Doctor Key</b></h1>
       <h2>Seu gerenciador de chaves automotivas</h2>
-        <Link href="/login">Login</Link>
+        <Link className='bg-green-500 py-2 px-7 text-white rounded hover:bg-green-600 shadow-lg' href="/login">Login</Link>
     </main>
   )
 }
