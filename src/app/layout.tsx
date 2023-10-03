73cb6effc43bc222c3d26cb33ef99ff45dc34196
gabeFrancisco <gabegit2@gmail.com>
2023-10-02 22:13:49 -0300

    changed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Josefin_Sans } from "next/font/google";
+import { Josefin_Sans, Lato, Open_Sans, Raleway, Ubuntu } from "next/font/google";
 import NextAuthSessionProvider from "./providers/NextAuthSessionProvider";
 
 import "@fortawesome/fontawesome-svg-core";
@@ -9,7 +9,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import { Providers } from "@/components/Provider/Provider";
 config.autoAddCss = false;
 
-const josefin = Josefin_Sans({ subsets: ["latin"] });
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
   title: "Doctor Key",
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body className={josefin.className}>
+      <body className={ubuntu.className}>
         <Providers>
           <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
         </Providers>
