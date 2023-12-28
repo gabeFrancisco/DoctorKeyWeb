ab68b1c843bc89e761c476bd093f84ddf3ae8064
gabeFrancisco <gabegit2@gmail.com>
2023-12-27 21:36:18 -0300

    re-styled landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
-import Image from 'next/image'
-import Logo from '../../public/drkey.png'
-import Link from 'next/link'
+import Image from "next/image";
+import Logo from "../../public/logo-white.svg";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image src={Logo} alt='Logo'/>
-      <h1 className='text-2xl'>Bem vindo ao <b>Doctor Key</b></h1>
-      <h2>Seu gerenciador de chaves automotivas</h2>
-        <Link className='bg-green-500 py-2 px-7 text-white rounded hover:bg-green-600 shadow-lg' href="/login">Login</Link>
+    <main className="flex min-h-screen  flex-col items-center justify-between landing-home">
+      <div className="p-28  ">
+        <Image src={Logo} className="w-6/12" alt="Logo" />
+        <h1 className="text-2xl my-10 text-white">
+          Bem vindo ao <b>Doctor Key</b>
+        </h1>
+        <h2 className="my-10 text-white">Seu gerenciador de chaves automotivas</h2>
+        <Link
+          className="bg-gradient-to-r from-gray-300 to-white py-2 px-7 text-green-500 rounded shadow-lg hover:border hover:border-gray-300"
+          href="/login"
+        >
+          Login
+        </Link>
+      </div>
     </main>
-  )
+  );
 }
