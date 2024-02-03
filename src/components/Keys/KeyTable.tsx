0203ab9e17e9a16c02e92167ac8e3bb1cf1fbe19
gabeFrancisco <gabegit2@gmail.com>
2024-02-03 18:11:40 -0300

    started advanced search of keys implementation with dropdown properties

--- src/components/Keys/KeyTable.tsx
+++ src/components/Keys/KeyTable.tsx
@@ -42,43 +42,45 @@ const KeyTable = () => {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <KeyToolbar handleClear={handleClear}/>
-      {isLoaded ? (
-        <div className="overflow-x-auto shadow-md rounded-sm w-full">
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-            <table className="w-full text-sm text-left text-gray-500 rounded-sm">
-              <thead className="border-0 rounded-sm text-md text-white bg-green-500">
-                <tr>
-                  <th className="px-2 lg:px-6 py-2 m-1">Modelo</th>
-                  <th className="px-2 lg:px-6 py-2 m-1">Marca</th>
-                  <th className="hidden lg:table-cell px-2 lg:px-6 py-2 m-1">
-                    Botões
-                  </th>
-                  <th className="hidden lg:table-cell px-2 lg:px-6 py-2 m-1">
-                    Ano
-                  </th>
-                  <th className="hidden lg:table-cell px-2 lg:px-6 py-2 m-1">
-                    Tipo
-                  </th>
-                  <th className="hidden lg:table-cell px-2 lg:px-6 py-2 m-1">
-                    Lâmina
-                  </th>
-                  <th className="px-2 lg:px-6 py-2 m-1">Preço</th>
-                  <th className="px-2 lg:px-6 py-2 m-1">Ações</th>
-                </tr>
-              </thead>
-              <tbody>
-                {keys.map((el, key) => (
-                  <KeyRow _key={el} />
-                ))}
-              </tbody>
-            </table>
-          </motion.div>
-        </div>
-      ) : (
-        <LoadingSkeleton />
-      )}
+    <div>
+      <KeyToolbar handleClear={handleClear} />
+      <div className="flex flex-col items-center">
+        {isLoaded ? (
+          <div className="overflow-x-auto shadow-md rounded-sm w-full">
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+              <table className="w-full text-sm text-left text-gray-500 rounded-sm">
+                <thead className="border-0 rounded-sm text-md text-white bg-green-500">
+                  <tr>
+                    <th className="px-2 lg:px-6 py-2 m-1">Modelo</th>
+                    <th className="px-2 lg:px-6 py-2 m-1">Marca</th>
+                    <th className="hidden lg:table-cell px-2 lg:px-6 py-2 m-1">
+                      Botões
+                    </th>
+                    <th className="hidden lg:table-cell px-2 lg:px-6 py-2 m-1">
+                      Ano
+                    </th>
+                    <th className="hidden lg:table-cell px-2 lg:px-6 py-2 m-1">
+                      Tipo
+                    </th>
+                    <th className="hidden lg:table-cell px-2 lg:px-6 py-2 m-1">
+                      Lâmina
+                    </th>
+                    <th className="px-2 lg:px-6 py-2 m-1">Preço</th>
+                    <th className="px-2 lg:px-6 py-2 m-1">Ações</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {keys.map((el, key) => (
+                    <KeyRow _key={el} />
+                  ))}
+                </tbody>
+              </table>
+            </motion.div>
+          </div>
+        ) : (
+          <LoadingSkeleton />
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Keys/KeyToolbar.tsx
+++ src/components/Keys/KeyToolbar.tsx
@@ -1,12 +1,16 @@
-import { searchKey } from '@/store/features/keySlice'
-import { useAppDispatch, useAppSelector } from '@/store/store'
-import { faPlus, faSearch, faSync } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Link from 'next/link'
-import React, { useState } from 'react'
+import bladeTypes from "@/models/constants/bladeTypes";
+import keyTypes from "@/models/constants/keyTypes";
+import manufactors from "@/models/constants/manufactors";
+import serviceTypes from "@/models/constants/serviceTypes";
+import { searchKey } from "@/store/features/keySlice";
+import { useAppDispatch, useAppSelector } from "@/store/store";
+import { faPlus, faSearch, faSync } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import React, { useState } from "react";
 
-interface KeyToolbarProps{
-  handleClear(): void,
+interface KeyToolbarProps {
+  handleClear(): void;
 }
 
 const KeyToolbar = (props: KeyToolbarProps) => {
@@ -26,43 +30,102 @@ const KeyToolbar = (props: KeyToolbarProps) => {
   };
 
   return (
-    <div className="rounded-md  w-full flex flex-row justify-between items-center my-1 py-2">
+    <div className="rounded-md my-1 py-2 flex flex-col lg:flex-row justify-between">
+      <div className="my-2 lg:my-0 flex flex-row items-center">
+        <Link
+          href="chaves/adicionar"
+          className="rounded-sm shadow-lg bg-green-500 py-1 px-3 text-sm text-white hover:bg-green-600"
+        >
+          {/* New key button */}
+          <FontAwesomeIcon icon={faPlus} className="mx-2" height={25} />
+          <span className="hidden md:inline-block">Nova chave!</span>
+        </Link>
         <div>
-          <Link
-            href="chaves/adicionar"
-            className="rounded-sm shadow-lg bg-green-500 py-2 px-3 text-sm text-white hover:bg-green-600"
+          <select
+            className="rounded-sm border-2 bg-gray-100 block mx-1 text-gray-700"
+            id="manufactor"
+            // value={}
+            // onChange={}
           >
-            {/* New key button */}
-            <FontAwesomeIcon icon={faPlus} className="mx-2" height={25} />
-            <span className="hidden md:inline-block">Nova chave!</span>
-          </Link>
+            {manufactors.map((el, key) => (
+              <option className="rounded-sm border-2 bg-gray-100" key={key}>
+                {el}
+              </option>
+            ))}
+          </select>
         </div>
-
-        <div className="flex flex-row items-stretch">
-          <input
-            type="text"
-            placeholder="Pesquisar por modelo"
-            className="rounded-md border-2 block w-52 lg:w-full p-0.5 text-sm"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button
-            onClick={handleSearch}
-            className="rounded-sm shadow-lg bg-green-500 mx-1 py-1 px-3 text-sm text-white hover:bg-green-600"
+        <div>
+          <select
+            className="rounded-sm border-2 bg-gray-100 block mx-1 text-gray-700"
+            id="keyType"
+            // value={}
+            // onChange={}
           >
-            <FontAwesomeIcon icon={faSearch} height={25} />
-          </button>
-          {search !== "" ? (
-            <button
-              onClick={props.handleClear}
-              className="rounded-sm shadow-lg bg-blue-500 mx-1 py-1 px-3 text-sm text-white hover:bg-blue-600"
-            >
-              <FontAwesomeIcon icon={faSync} height={25} />
-            </button>
-          ) : null}
+            {keyTypes.map((el, key) => (
+              <option className="rounded-sm border-2 bg-gray-100" key={key}>
+                {el}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <select
+            className="rounded-sm border-2 bg-gray-100 block mx-1 text-gray-700"
+            id="bladeType"
+            // value={}
+            // onChange={}
+          >
+            {bladeTypes.map((el, key) => (
+              <option className="rounded-sm border-2 bg-gray-100" key={key}>
+                {el}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <select
+            className="rounded-sm border-2 bg-gray-100 block mx-1 text-gray-700"
+            id="serviceType"
+            // value={}
+            // onChange={}
+          >
+            {serviceTypes.map((el, key) => (
+              <option className="rounded-sm border-2 bg-gray-100" key={key}>
+                {el}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
-  )
-}
 
-export default KeyToolbar
+      <div className="flex flex-row items-stretch">
+        <input
+          type="text"
+          placeholder="Pesquisar por modelo"
+          className="rounded-md border-2 block w-52 lg:w-full p-0.5 text-sm"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          onClick={handleSearch}
+          className="rounded-sm shadow-lg bg-green-500 mx-1 py-1 px-3 text-sm text-white hover:bg-green-600"
+        >
+          <FontAwesomeIcon icon={faSearch} height={25} />
+        </button>
+        {search !== "" ? (
+          <button
+            onClick={() => {
+              props.handleClear();
+              setSearch("");
+            }}
+            className="rounded-sm shadow-lg bg-blue-500 mx-1 py-1 px-3 text-sm text-white hover:bg-blue-600"
+          >
+            <FontAwesomeIcon icon={faSync} height={25} />
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default KeyToolbar;
