e543ba7fd919a3eac1d44f52da44801f4ee72eaa
gabeFrancisco <gabegit2@gmail.com>
2024-05-01 16:19:10 -0300

    added bell icon on UserProfile component

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,31 +1,42 @@
-'use client'
+"use client";
 
 import useOutsideClick from "@/hooks/useOutsideClick";
 import React, { useState } from "react";
 import Image from "next/image";
 import User from "../../../public/user.png";
 import { signOut, useSession } from "next-auth/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 const UserProfile = () => {
   const [isDrop, setIsDrop] = useState(false);
   const ref = useOutsideClick(setIsDrop);
   const { data, status } = useSession();
   return (
-    <div
-      className="cursor-pointer flex flex-row justify-center hover:bg-green-300 hover:rounded-lg py-1 px-3"
-      onClick={() => (isDrop ? setIsDrop(false) : setIsDrop(true))}
-    >
-      <Image
-        className="rounded-full border border-white w-8 h-8"
-        src={User}
-        alt="User image"
-      />
-      <div className="flex flex-row items-center justify-center">
-        {status === "authenticated" && data !== null && (
-          <div className="flex flex-row items-center">
-            <p className="mx-3 text-sm">{data.user.name}</p>
-          </div>
-        )}
+    <div className="cursor-pointer flex flex-row justify-center items-center">
+      <div className="hover:bg-green-300 rounded-lg p-1 mx-2">
+        <small className="text- absolute top-1 bg-red-400 px-1 rounded-full scale-90">1</small>
+        <FontAwesomeIcon
+          className="text-center ml-1  text-2xl"
+          icon={faBell}
+        />
+      </div>
+      <div
+        className="flex flex-row p-1 hover:bg-green-300 rounded-lg"
+        onClick={() => (isDrop ? setIsDrop(false) : setIsDrop(true))}
+      >
+        <Image
+          className="rounded-full border block border-white w-8 h-8 max-w-lg"
+          src={User}
+          alt="User image"
+        />
+        <div className="flex flex-row items-center mx-2">
+          {status === "authenticated" && data !== null && (
+            <div className="items-center">
+              <p className="text-sm">{data.user.name}</p>
+            </div>
+          )}
+        </div>
       </div>
       {isDrop ? (
         <div ref={ref} className="">
