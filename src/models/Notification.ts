11348361e2810b3a47fffd765e5136b911a1e378
gabeFrancisco <gabegit2@gmail.com>
2024-05-01 21:31:17 -0300

    Removed User and changed to username prop on Notification

--- src/models/Notification.ts
+++ src/models/Notification.ts
@@ -1,7 +1,10 @@
 import { BaseEntity } from "./BaseEntity";
+import { User } from "./User";
 
-export interface Notification extends BaseEntity{
+export interface Notification extends BaseEntity {
   title: string;
   message: string;
+  readed: boolean;
+  user: User;
   workGroupId: string;
 }
---
+++ src/models/User.ts
@@ -0,0 +1,8 @@
+import { BaseEntity } from "./BaseEntity";
+
+export interface User extends BaseEntity {
+  username: string;
+  name: string;
+  email: string;
+  role: "Admin" | "Worker" | null;
+}
