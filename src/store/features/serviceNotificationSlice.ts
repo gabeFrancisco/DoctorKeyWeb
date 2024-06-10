8afbe2349f9228c0b148ffbe6b94152598b88784
gabeFrancisco <gabegit2@gmail.com>
2024-06-09 23:47:01 -0300

    ServiceNotificationSlice setup

--- src/store/features/serviceNotificationSlice.ts
+++ src/store/features/serviceNotificationSlice.ts
@@ -1,4 +1,31 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import api from "@/services/api";
+import { ServiceNotification } from "@/hooks/ServiceNotification";
 
-interface ServiceNotificationSlice {}
+interface ServiceNotificationState {
+  serviceNotificatoin: ServiceNotification;
+  serviceNotificationList: Array<ServiceNotification>;
+}
+
+const initialState: ServiceNotificationState = {
+  serviceNotificatoin: {
+    id: "",
+    title: "",
+    message: "",
+    priority: "",
+    state: "",
+    username: "",
+    workGroupId: "",
+  },
+  serviceNotificationList: new Array<ServiceNotification>(),
+};
+
+export const ServiceNotificationSlice = createSlice({
+  name: "ServiceNotifications",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export default ServiceNotificationSlice.reducer;
+export const serviceNotificationActions = ServiceNotificationSlice.actions;
